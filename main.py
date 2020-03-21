b9ff9adb65a2d46a38e07a2c4d6b12c0cface66f
yxb <xiangbo_x@qq.com>
2020-03-21 20:38:52 +0800

    能查信息了

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 # -*- coding: utf-8 -*-
 
-# import requests
-# from bs4 import BeautifulSoup
-# import webbrowser
-from wox import Wox
+import requests
+from bs4 import BeautifulSoup
+import webbrowser
+from wox import Wox, WoxAPI
 
 class DictClass(Wox):
 
     def query(self,query):
-        # proxies = {}
-        # if self.proxy and self.proxy.get("enabled") and self.proxy.get("server"):
-        #     proxies = {
-        #       "http": "http://{}:{}".format(self.proxy.get("server"),self.proxy.get("port")),
-        #       "http": "https://{}:{}".format(self.proxy.get("server"),self.proxy.get("port"))
-        #     }
+        proxies = {}
+        if self.proxy and self.proxy.get("enabled") and self.proxy.get("server"):
+            proxies = {
+              "http": "http://{}:{}".format(self.proxy.get("server"),self.proxy.get("port")),
+              "http": "https://{}:{}".format(self.proxy.get("server"),self.proxy.get("port"))
+            }
             #self.debug(proxies)
+
+        url = 'http://dict.baidu.com/s?wd=' + query
+        r = requests.get(url, proxies = proxies)
+        bs = BeautifulSoup(r.text)
+        c = bs.select('#empty-body')
         results = []
-        results.append({
-            "Title": "Hello World",
-            "SubTitle": "Query: {}".format(query),
-            "IcoPath":"Images/app.ico",
-            "ContextData": "ctxData"
-        })
+        for i in c:
+            results.append({
+                "Title": i.find('strong').text,
+                "SubTitle": i.find('p').text,
+                "IcoPath":"Images/app.ico",
+                # "ContextData": "ctxData",
+                "JsonRPCAction":{
+                    "method": "openUrl",
+                    "parameters":[url],
+                    "dontHideAfterAction":True
+                }
+            })
         return results
         # r = requests.get('https://news.ycombinator.com/',proxies = proxies)
         # bs = BeautifulSoup(r.text)
@@ -33,10 +44,10 @@ class DictClass(Wox):
 
         # return results
 
-    # def openUrl(self,url):
-    #     webbrowser.open(url)
-    #     #todo:doesn't work when move this line up 
-    #     WoxAPI.change_query(url)
+    def openUrl(self,url):
+        webbrowser.open(url)
+        #todo:doesn't work when move this line up 
+        WoxAPI.change_query(url)
     
     def context_menu(self, data):
         results = []
