ab59f709d7485d0fee68b2da00cdfaf3e6a3efc2
yxb <xiangbo_x@qq.com>
2020-03-22 02:14:49 +0800

    bug fix, and click to open url

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 import requests
 from bs4 import BeautifulSoup
-from wox import Wox
+import webbrowser
+from wox import Wox,WoxAPI
 
 class DictClass(Wox):
 
@@ -18,17 +19,41 @@ class DictClass(Wox):
         url = 'http://dict.baidu.com/s?wd=' + query
         r = requests.get(url, proxies = proxies)
         bs = BeautifulSoup(r.text)
-        c = bs.select('#empty-body')
+        #c = bs.select('#empty-body')
+        c = bs.select('.tab-content')
         results = []
-        for i in c:
+        for index, i in enumerate(c):
+            if not i.find('p'):
+                break
             results.append({
-                "Title": i.find('strong').text,
-                "SubTitle": i.find('p').text,
+                "Title": str(index+1)+":",
+                "SubTitle": i.find('p').text.strip().replace("\n",""),
                 "IcoPath":"Images/app.ico",
-                "ContextData": "ctxData",
+                "JsonRPCAction":{
+                #You can invoke both your python functions and Wox public APIs .
+                #If you want to invoke Wox public API, you should invoke as following format: Wox.xxxx
+                #you can get the public name from https://github.com/qianlifeng/Wox/blob/master/Wox.Plugin/IPublicAPI.cs,
+                #just replace xxx with the name provided in this url
+                "method": "openUrl",
+                #you MUST pass parater as array
+                "parameters":[url],
+                #hide the query wox or not
+                "dontHideAfterAction":True
+                }
+            })
+        if not results and query:
+            results.append({
+                "Title": query,
+                "SubTitle": "未能找到数据",
+                "IcoPath":"Images/app.ico",
+                "ContextData": "ctxData"
             })
         return results
-    
+
+    def openUrl(self,url):
+        webbrowser.open(url)
+        #WoxAPI.change_query(url)
+
     def context_menu(self, data):
         results = []
         results.append({
